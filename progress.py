dcd0a57c50e916dcd812734978a6fe3643425388
2 Young 2 Simple! orz <lfh010618@gmail.com>
2022-07-04 11:21:39 +0800

    feat: year and month change to specific date

--- progress.py
+++ progress.py
@@ -1,13 +1,13 @@
-# author: @bGZoCg
-# update: 2022/2/8
+# author: @bGZo
+# update: 220704
 # More social medias here:
-# https://twitter.com/decade_progress
-# https://twitter.com/year_progress
+    # https://twitter.com/decade_progress
+    # https://twitter.com/year_progress
 
 from datetime import date
 
 HEARDER = '''\
-![](https://komarev.com/ghpvc/?username=bGZoCg) <img src="https://img.shields.io/github/last-commit/bgzo/blog?color=ff69b4&label=blog%20update%20%40%20"/> 
+![](https://komarev.com/ghpvc/?username=bGZoCg) <a href = "http://blog.bgzo.cc"><img src="https://img.shields.io/github/last-commit/bgzo/blog?color=ff69b4&label=blog%20update%20%40%20"/></a>
 
 <img align="right" width="30%" src="https://media.giphy.com/media/k8kITi9SAwe9JWbUaH/giphy.gif">
 
@@ -25,7 +25,8 @@ PROFILE='''\
 + 🎯 diversity is essential to happiness
 ```
 
-- [2021](https://github.com/bGZoCg/2021)
+- [My 2021](https://github.com/bGZoCg/2021)
+- [My 2022](https://github.com/bGZoCg/2022)
 '''
 
 
@@ -57,23 +58,25 @@ def days_till_now():
     return days
 
 def draw_year_progress_bar():
-    tmp = ' Year Passed '
+    today = date.today()
+    tmp = '{:4}'.format(today.year) + ' Passed '
     days = days_till_now()
-    for i in range(0, round(days*15/days_in_year(date.today().year))):
+    for i in range(0, round(days*15/days_in_year(today.year))):
         tmp += "▓"
-    for i in range(round(days*15/days_in_year(date.today().year)), 15):
+    for i in range(round(days*15/days_in_year(today.year)), 15):
         tmp += "░"
-    tmp += ' ' + str(round(days_till_now()/days_in_year(date.today().year)*100, 2)) + '%'
+    tmp += ' ' + str(round(days_till_now()/days_in_year(today.year)*100, 2)) + '%'
     return tmp
 
 def draw_month_progress_bar():
-    tmp = 'Month Passed '
-    days = date.today().day
+    today = date.today()
+    tmp = '{:4}'.format(today.strftime('%b')) + ' Passed '
+    days = today.day
     for i in range(0, round(days/2)):
         tmp += "▓"
     for i in range(round(days/2), 15):
         tmp += "░"
-    tmp += ' ' + str(round(days*100/days_in_month(date.today().year, date.today().month))) + '%'
+    tmp += ' ' + str(round(days*100/days_in_month(today.year, today.month))) + '%'
     return tmp
 
 def write_file():
